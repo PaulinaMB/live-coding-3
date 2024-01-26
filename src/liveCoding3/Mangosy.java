9cade73844bc11d62490d402d6ed8e883f489a09
PaulinaMB <paulinamendozabernal@gmail.com>
2024-01-26 14:21:44 -0600

    Ejercicio desarrollado para el ejercicio 3 de live coding

---
+++ src/liveCoding3/Mangosy.java
@@ -0,0 +1,27 @@
+package liveCoding3;
+
+import java.util.Scanner;
+
+package liveCoding4;
+/*Retroalimentación: La desarrolladora presento dificultades, tuvo su camara encendida y explico el código conforme lo fue haciendo.*/
+
+
+public class Mangosy {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner (System.in);
+		System.out.println("Ingresa el número de mangos: ");
+		int mangos = scanner.nextInt();
+		System.out.println("Ingresa el número de naranjas: ");
+		int naranjas = scanner.nextInt();
+		if(naranjas%mangos == 0) {
+			//int cajas =
+		}
+		
+		int cajas = Math.max(mangos, naranjas);
+		
+		
+		scanner.close();
+		
+	}
+	
+}
